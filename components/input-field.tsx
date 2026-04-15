ce162aeb7795a41d2f2d535124513117351a4cec
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-15 13:26:12 +0200

    Made TextInput into custom component

---
+++ components/input-field.tsx
@@ -0,0 +1,41 @@
+import { TextInput, useColorScheme, StyleSheet, useWindowDimensions } from 'react-native';
+import { Colors } from '@/constants/theme'
+
+type inputProps = {
+    name: string;
+    value: string;
+    onChange: (text: string) => void;
+}
+
+export function inputField({ name, value, onChange }: inputProps) {
+    const colorScheme = useColorScheme();
+    const themeColors = Colors[colorScheme ?? 'light'];
+
+    const { width, height } = useWindowDimensions();
+
+    const inputHeight = Math.round(height * 0.06);
+
+    return (
+        <TextInput
+            placeholder={name}
+            placeholderTextColor={themeColors.icon}
+            style={[styles.inputBox, styles.inputBoxPortrait, { backgroundColor: themeColors.card, color: themeColors.text, height: inputHeight }]}
+            value={value}
+            onChangeText={onChange}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    inputBox: {
+        marginBottom: 18,
+        paddingHorizontal: 14,
+        borderRadius: 12,
+        fontSize: 16,
+        maxWidth: 380,
+    },
+    inputBoxPortrait: {
+        maxWidth: undefined,
+        width: '100%',
+    }
+});
